58b3f240d6ad815ea98cb54fe6cdc72836c3288f
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2020-01-21 12:15:53 +0800

    如果採用，此提交將更新開發環境所需要的套件

---
+++ AutoInstall.sh
@@ -0,0 +1,12 @@
+pip install -r requirements.txt --upgrade
+
+apm install linter-pylint
+apm install python-linters
+apm install linter-pycodestyle
+apm install linter-pydocstyle
+apm install linter-flake8
+apm install linter-pylama
+apm install linter-mypy
+apm install linter-vhdl
+apm install linter-travis-lint
+apm install linter-js-yaml
