aed081b5e25635e3c835e7ac6f37659c42b1be81
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2020-05-14 08:25:51 +0800

    如果採用，此提交將更新腳本內容，雖然還是會出現執行錯誤的問題
    修改項目:
    
    模組:
    
    議題

--- Docker.sh
+++ Docker.sh
@@ -1,22 +1,21 @@
 # 安裝docker
-apt-get remove docker docker-engine docker.io containerd runc -y
-apt-get update -y
-apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
-apt-get update -y
-apt-get install docker-ce docker-ce-cli containerd.io -y
-apt-cache madison docker-ce
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-cache madison docker-ce
 
 # 設定一般權限
-groupadd docker
-usermod -aG docker $USER
-newgrp docker
-docker run hello-world
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo newgrp docker
 
 # docker開機自動啟動
-systemctl enable docker
+sudo systemctl enable docker
 
 # 測試Hello World
 docker run hello-world
