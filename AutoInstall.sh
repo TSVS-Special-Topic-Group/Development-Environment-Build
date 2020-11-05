b6e5eda5bba9beaeeee5d5df81627cbd2a8e5adf
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2020-11-05 16:53:40 +0800

    將針對使用的開發環境需要的程式與軟體做自動化的安裝

--- AutoInstall.sh
+++ AutoInstall.sh
@@ -1,18 +1,41 @@
 # 系統更新與升級
-apt-get update -y
-apt-get install python3.6 python3.6-dev python3-pip -y
-apt-get upgrade -y
+sudo apt-get update -y
+sudo apt-get install python3 python3-dev python3-pip -y -f
+sudo apt-get install git ffmpeg libxss1 xdg-utils -y -f
+sudo apt install iverilog verilator cmake -y -f
+sudo apt-get upgrade -y
+
+# 安裝Kit
+wget -O kite-installer https://linux.kite.com/dls/linux/current
+bash kite-installer --download
+bash kite-installer --install
+
+# 安裝slang
+git clone https://github.com/MikePopoloski/slang.git
+cd slang
+mkdir build && cd build
+cmake -DCMAKE_CXX_COMPILER=g++-9 -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local/bin ..
+make -j8
+ctest
+bin/unittests
+sudo make install
+cd ../../
 
 # Python套件安裝
-python3 -m pip install -r requirements.txt --upgrade
+sudo python3 -m pip install -r requirements.txt --upgrade
 
-# Atom套件權限設定
-chown -R $USER:$USER ~/.atom
+# 安裝Dulwich
+git clone https://github.com/dulwich/dulwich.git
+cd dulwich/
+python setup.py --pure install
+cd ..
 
-# Python檢查套件
+# Atom UI
 apm install linter
 apm install linter-ui-default
 apm install intentions
+
+# Python linter
 apm install python-linters
 
 # Atom Python自動填寫
@@ -20,16 +43,17 @@ apm install Hydrogen
 apm install kite
 apm install autocomplete-python
 
-# Atom繁體中文
+# Atom 繁體中文
 apm install cht-menu
 
 # Atom效能檢查
 apm install busy-signal
 
 # Atom Markdown
-apm install pdf-view
-apm install markdown-preview-enhanced
-apm install markdown-table-editor
-apm install insert-timestamp
-apm install markdown-image-insertion
-apm install markdown-image-assistant
+apm install pdf-view markdown-preview-enhanced markdown-table-editor insert-timestamp markdown-image-insertion markdown-image-assistant
+
+# Atom HDL linter
+apm install linter-veriloghdl linter-vhdl
+
+# Atom HDL language support
+apm install language-verilog language-vhdl lancelot-language-hdl
