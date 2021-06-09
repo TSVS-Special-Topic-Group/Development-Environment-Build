8773fb1e2d2257ae09bbe6fb5a50a9a161ba61a5
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-06-10 00:28:48 +0800

    如果採用，此提交將新增空白行
    修改項目:
    
    模組:
    
    議題

--- Anbox.sh
+++ Anbox.sh
@@ -2,6 +2,9 @@ sudo modprobe ashmem_linux
 sudo modprobe binder_linux
 sudo ls -1 /dev/{ashmem,binder}
 snap install --devmode --beta anbox
+
 wget https://raw.githubusercontent.com/geeks-r-us/anbox-playstore-installer/master/install-playstore.sh
+
 chmod +x install-playstore.sh
+
 ./install-playstore.sh
