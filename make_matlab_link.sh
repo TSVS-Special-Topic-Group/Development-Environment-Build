362cceeca28c1a6199111b74f7e973881bcdbf71
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-06-09 23:10:05 +0800

    如果採用，此提交將更新呼叫MATLAB方式並製作其他程式呼叫
    修改項目:
    
    模組:
    
    議題

---
+++ make_matlab_link.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# 建立一個MATLAB連結檔在/usr/bin/
+cd /usr/bin
+sudo ln -s /usr/local/MATLAB/R2020a/bin/deploytool deploytool
+sudo ln -s /usr/local/MATLAB/R2020a/bin/matlab matlab
+sudo ln -s /usr/local/MATLAB/R2020a/bin/mbuild mbuild
+sudo ln -s /usr/local/MATLAB/R2020a/bin/mcc mcc
+sudo ln -s /usr/local/MATLAB/R2020a/bin/mex mex
