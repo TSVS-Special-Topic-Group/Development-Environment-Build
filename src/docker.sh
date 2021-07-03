624ae1b1c8232cb21ca640c6d58374400adb3282
timmy61109 <38396747+timmy61109@users.noreply.github.com>
2021-07-03 18:34:31 +0800

    如果採用，此提交將更新Docker安裝腳本
    修改項目:
    
    模組:
    
    議題

--- src/docker.sh
+++ src/docker.sh
@@ -1,19 +1,13 @@
 # 安裝docker
 if docker > /dev/null
   then
-    sudo apt-get update -y
-    sudo apt-get upgrade -y
     sudo apt-get remove docker docker-engine docker.io containerd runc -y
-    sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y -f
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo apt-key fingerprint 0EBFCD88
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    echo \
-      "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y -f
+    sudo sh -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg'
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update -y
     sudo apt-get install docker-ce docker-ce-cli containerd.io -y -f
-    sudo apt-cache madison docker-ce -y
+    sudo apt-get upgrade -y
   else
     curl -fsSL https://get.docker.com -o get-docker.sh
     sudo sh get-docker.sh
