d44c1b0eac699c19c91b521886552270d8234af4
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-10-14 19:16:18 +0800

    更新Docker腳本，以官方線上直接安裝

--- src/docker.sh
+++ src/docker.sh
@@ -1,23 +1,23 @@
 # 安裝docker
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y -f
+sudo sh -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg'
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y -f
+sudo apt-get upgrade -y
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+sudo systemctl enable docker
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
 if docker > /dev/null
   then
-    sudo apt-get remove docker docker-engine docker.io containerd runc -y
-    sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y -f
-    sudo sh -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg'
-    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-    sudo apt-get update -y
-    sudo apt-get install docker-ce docker-ce-cli containerd.io -y -f
-    sudo apt-get upgrade -y
+    echo "You installed Docker."
+    docker run hello-world
   else
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    sudo sh get-docker.sh
-    sudo groupadd docker
-    sudo usermod -aG docker $USER
-
-    sudo systemctl enable docker
-    sudo systemctl enable docker.service
-    sudo systemctl enable containerd.service
-
+    echo "You not installed Docker."
   fi
-
-docker run hello-world
