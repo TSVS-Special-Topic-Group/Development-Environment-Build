280c7ad7333bf79cfc76a226b2dc6efddb178f5c
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2021-11-12 23:57:37 +0800

    此提交將更新腳本內容
    修改項目:
    
    模組:
    
    議題

--- src/autoinstall.bat
+++ src/autoinstall.bat
@@ -1,3 +1,4 @@
 chcp 65001
-pip install -r requirements.txt --upgrade
-apm install linter linter-ui-default intentions python-linters Hydrogen kite autocomplete-python cht-menu busy-signal pdf-view markdown-preview-enhanced markdown-table-editor insert-timestamp markdown-image-insertion markdown-image-assistant linter-veriloghdl linter-vhdl language-verilog language-vhdl lancelot-language-hdl
+cd ..
+pip install -r requirements.txt --upgradeapm install linter
+apm install linter-ui-default intentions python-linters Hydrogen kite autocomplete-python cht-menu busy-signal pdf-view markdown-preview-enhanced markdown-table-editor insert-timestamp markdown-image-insertion markdown-image-assistant linter-veriloghdl linter-vhdl language-verilog language-vhdl lancelot-language-hdl language-hdl linter-gcc gpp-compiler linter-docker docker language-docker platformio-ide-terminal
