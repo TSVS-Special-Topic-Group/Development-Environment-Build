b4079f48e91eb0a28bc4b092ba7eeec780621102
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-01-13 11:46:02 +0800

    此提交將新增R環境與整合編輯器
    修改項目:
    
    模組:
    
    議題

--- src/auto-install.sh
+++ src/auto-install.sh
@@ -45,6 +45,23 @@ sudo apt-get install python3-pyaudio portaudio19-dev -y -f
 sudo apt-get install build-essential swig libpulse-dev libasound2-dev  -y -f
 sudo apt-get install -y zlib1g-dev libbz2-dev liblzma-dev libboost-all-dev -y -f
 
+# R
+# update indices
+sudo apt update -qq
+# install two helper packages we need
+sudo apt install --no-install-recommends software-properties-common dirmngr
+# add the signing key (by Michael Rutter) for these repos
+# To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+# Fingerprint: 298A3A825C0D65DFD57CBB651716619E084DAB9
+sudo wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+# add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
+sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
+sudo add-apt-repository ppa:c2d4u.team/c2d4u4.0+ -y
+
+sudo apt-get install libatlas3-base -y -f
+sudo apt-get install libopenblas-base -y -f
+sudo apt-get install r-base r-base-dev -y -f
+
 # playonlinux
 sudo apt-get install playonlinux -y -f
 
