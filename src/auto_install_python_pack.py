f936d01ff923f8266aa86493bfeb447d604a5008
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-09-20 22:35:16 +0800

    修改程式讓她自動判斷使用哪一種版本號

--- src/auto_install_python_pack.py
+++ src/auto_install_python_pack.py
@@ -1,14 +1,23 @@
 """Auto Install python package."""
 from os import system
-
+import sys
 
 system("echo > auto_install_python_pack.log")
+PACKAGE = "pip"
 
 with open('requirements.txt', 'r', encoding='utf8') as file:
+    print(sys.version)
     for line in file.readlines():
         if line != "\n" and line[:1] != "#":
-            print(line[:-1])
+            if sys.version[:3] == "3.8" and line[-13:-1] == " # python3.8":
+                PACKAGE = line[:-13]
+            elif line[-13:-4] != " # python":
+                PACKAGE = line[:-1]
+            else:
+                PACKAGE = "pip"
+
+            print(PACKAGE)
             system(
                 "python3 -m pip install " +
-                line[:-1] + " --upgrade --no-warn-script-location" +
+                PACKAGE + " --upgrade --no-warn-script-location" +
                 " >> auto_install_python_pack.log")
