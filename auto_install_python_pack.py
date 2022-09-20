c3b258f1205a50381b6c59f42adbe0e3a298a7e2
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-09-20 13:29:09 +0800

    加入將資訊寫入到指定檔案

--- auto_install_python_pack.py
+++ auto_install_python_pack.py
@@ -2,10 +2,13 @@
 from os import system
 
 
+system("echo > auto_install_python_pack.log")
+
 with open('requirements.txt', 'r', encoding='utf8') as file:
     for line in file.readlines():
         if line != "\n" and line[:2] != "# ":
             print(line[:-1])
             system(
                 "python3 -m pip install " +
-                line[:-1] + " --upgrade --no-warn-script-location")
+                line[:-1] + " --upgrade --no-warn-script-location" +
+                " >> auto_install_python_pack.log")
