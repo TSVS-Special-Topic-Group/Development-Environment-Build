0ba98666880e276d4ce58df60b08386223ef2edf
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-09-20 12:45:59 +0800

    更新縮排位置

--- auto-install-python-pack.py
+++ auto-install-python-pack.py
@@ -6,5 +6,6 @@ with open('requirements.txt', 'r') as file:
     for line in file.readlines():
         if line != "\n":
             print(line[:-1])
-            system("python3 -m pip install "
-                + line[:-1] + " --upgrade --no-warn-script-location")
+            system(
+                "python3 -m pip install " +
+                line[:-1] + " --upgrade --no-warn-script-location")
