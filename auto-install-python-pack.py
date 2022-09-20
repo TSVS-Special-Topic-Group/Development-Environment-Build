9db0bcde22a268ca816de90dcc5ae390a223f2da
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-09-20 12:03:09 +0800

    此提交將使程式可以自動更新套件並安裝
    修改項目:
    
    模組:
    
    議題

---
+++ auto-install-python-pack.py
@@ -0,0 +1,10 @@
+"""Auto Install python package."""
+from os import system
+
+
+with open('requirements.txt', 'r') as file:
+    for line in file.readlines():
+        if line != "\n":
+            print(line[:-1])
+            system("python3 -m pip install "
+                + line[:-1] + " --upgrade --no-warn-script-location")
