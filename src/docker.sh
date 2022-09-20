75a7fb6312b756e5577feea24fd2e8bee46f2cbc
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-09-20 23:03:33 +0800

    更新新的docker網址與腳本

--- src/docker.sh
+++ src/docker.sh
@@ -1,10 +1,13 @@
 # 安裝docker
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release -y -f
-sudo sh -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg'
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y -f
+sudo apt-get install ca-certificates curl gnupg lsb-release -y -f
+sudo sh -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg'
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y -f
 sudo apt-get upgrade -y
 
 sudo groupadd docker
