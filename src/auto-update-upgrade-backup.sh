e2ac90b978b7258303a8f40fd278e77e3ac752d0
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-09-24 20:44:55 +0800

    更新開啟的Port

--- src/auto-update-upgrade-backup.sh
+++ src/auto-update-upgrade-backup.sh
@@ -10,7 +10,7 @@ sudo apt-get update
 sudo apt-get upgrade -y
 docker pull gitlab/gitlab-ce:latest
 
-docker run -d --publish 443:443 --publish 80:80 --publish 22:22 --name gitlab --restart always --volume /var/lib/gitlab/config/:/etc/gitlab --volume /var/lib/gitlab/logs/:/var/log/gitlab --volume /var/lib/gitlab/data/:/var/opt/gitlab gitlab/gitlab-ce:latest
+docker run -d --publish 443:443 --publish 80:80 --publish 22:22 --publish 25:25 --publish 465:465 --publish 587:587 --name gitlab --restart always --volume /var/lib/gitlab/config/:/etc/gitlab --volume /var/lib/gitlab/logs/:/var/log/gitlab --volume /var/lib/gitlab/data/:/var/opt/gitlab gitlab/gitlab-ce:latest
 minikube start --cpus=2
 kubectl create namespace gitlab
 helm repo update
