4bc968a94d83da8fa0a7e22e5c495634667f2873
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-09-24 20:44:17 +0800

    此提交將新增自動安裝GitLab腳本

---
+++ src/install-gitlab-ce.sh
@@ -0,0 +1,43 @@
+nohup noip-duc -g atca.ddns.net -u timmy61109 -p 123WwDdVv_ &
+
+if docker > /dev/null
+  then
+    echo "You installed Docker."
+  else
+    echo "You not installed Docker."
+    sh docker.sh
+  fi
+
+
+# GitLab-CE
+docker run -d --publish 443:443 --publish 80:80 --publish 22:22 --publish 25:25 --publish 465:465 --publish 587:587 --name gitlab --restart always --volume /var/lib/gitlab/config/:/etc/gitlab --volume /var/lib/gitlab/logs/:/var/log/gitlab --volume /var/lib/gitlab/data/:/var/opt/gitlab gitlab/gitlab-ce:latest
+docker restart gitlab
+
+# Restore
+# sudo cp *_gitlab_backup.tar /var/lib/gitlab/data/backups/
+# sudo cp <備份版本>_gitlab_backup.tar /var/lib/gitlab/data/backups
+# docker exec -it gitlab bash
+# chown git:git /var/opt/gitlab/backups/<備份版本>_gitlab_backup.tar
+# exit
+# docker exec -it <name of container> gitlab-backup restore BACKUP=<備份版本>
+sudo cp gitlab.rb /var/lib/gitlab/config/
+# sudo cp gitlab-secrets.json /var/lib/gitlab/config/
+docker restart gitlab
+
+# Kubectl 
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl
+sudo apt-get install -y apt-transport-https
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# Minikube
+https://github.com/kubernetes/minikube/releases/download/v1.27.0/minikube_1.27.0-0_amd64.deb
+sudo dpkg -i minikube_*.deb
+minikube start --driver=docker
+minikube config set driver docker
+
+# Helm
+sudo snap install helm --classic
