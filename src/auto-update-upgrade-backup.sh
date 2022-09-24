18227e8dac6cb52379d61d9c737e52abd798bbb4
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2022-09-25 00:20:14 +0800

    更新使用核心數

--- src/auto-update-upgrade-backup.sh
+++ src/auto-update-upgrade-backup.sh
@@ -11,7 +11,7 @@ sudo apt-get upgrade -y
 docker pull gitlab/gitlab-ce:latest
 
 docker run -d --publish 443:443 --publish 80:80 --publish 22:22 --publish 25:25 --publish 465:465 --publish 587:587 --name gitlab --restart always --volume /var/lib/gitlab/config/:/etc/gitlab --volume /var/lib/gitlab/logs/:/var/log/gitlab --volume /var/lib/gitlab/data/:/var/opt/gitlab gitlab/gitlab-ce:latest
-minikube start --cpus=2
+minikube start --cpus=6
 kubectl create namespace gitlab
 helm repo update
 helm install --namespace gitlab gitlab-runner -f values.yaml gitlab/gitlab-runner
