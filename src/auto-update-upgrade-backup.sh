c3edf74a4b6bcc90401c888f0a43f2a8488e8f13
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2023-02-22 23:35:17 +0800

    Relate # ! 提交將加入一些更新，協助GitLab Runner 架設
    修改項目:
    
    模組:

--- src/auto-update-upgrade-backup.sh
+++ src/auto-update-upgrade-backup.sh
@@ -7,7 +7,7 @@ docker stop gitlab
 docker rm gitlab
 helm delete gitlab-runner -n gitlab
 minikube stop
-minikube delete
+minikube delete --all --purge
 
 sudo apt-get update
 sudo apt-get upgrade -y
@@ -16,10 +16,10 @@ sudo ddclient -daemon=0 -debug -verbose -noquiet
 # sudo dpkg -i minikube_*.deb
 docker pull gitlab/gitlab-ce:latest
 
- docker run --cpus=6 --cpuset-cpus 0-5 -d --publish 80:80 --publish 443:443 --publish 22:22 --publish 25:25 --publish 465:465 --publish 587:587 --name gitlab --restart always --volume /var/lib/gitlab/config/:/etc/gitlab --volume /var/lib/gitlab/logs/:/var/log/gitlab --volume /var/lib/gitlab/data/:/var/opt/gitlab gitlab/gitlab-ce:15.7.7-ce.0
+docker run --cpus=6 --cpuset-cpus 0-5 -d --publish 80:80 --publish 443:443 --publish 22:22 --publish 25:25 --publish 465:465 --publish 587:587 --name gitlab --restart always --volume /var/lib/gitlab/config/:/etc/gitlab --volume /var/lib/gitlab/logs/:/var/log/gitlab --volume /var/lib/gitlab/data/:/var/opt/gitlab gitlab/gitlab-ce:15.7.7-ce.0
 # docker run --cpus=1 --publish 80:80 --name -it -d atca python:latest
 
-minikube start --cpus=6 --force
+minikube start --cpus 4 --force --force-systemd=true --driver=docker
 kubectl create namespace gitlab
 helm repo update
 helm install --namespace gitlab gitlab-runner -f gitlab-values.yaml gitlab/gitlab-runner
