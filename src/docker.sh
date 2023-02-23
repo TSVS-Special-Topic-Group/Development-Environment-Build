11b5623695c53d25e1724a7981934239824b2e3d
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2023-02-23 17:04:08 +0800

    Relate # ! 提交將刪除sudo 以提供自動化測試
    修改項目:
    
    模組:

--- src/docker.sh
+++ src/docker.sh
@@ -1,21 +1,21 @@
 # 安裝docker
-sudo apt-get remove docker docker-engine docker.io containerd runc -y
-sudo apt-get update -y
-sudo apt-get install ca-certificates curl gnupg lsb-release -y -f
-sudo sh -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg'
+apt-get remove docker docker-engine docker.io containerd runc -y
+apt-get update -y
+apt-get install ca-certificates curl gnupg lsb-release -y -f
+sh -c 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg'
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y -f
-sudo apt-get upgrade -y
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update -y
+apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y -f
+apt-get upgrade -y
 
-sudo groupadd docker
-sudo usermod -aG docker $USER
+groupadd docker
+usermod -aG docker $USER
 
-sudo systemctl enable docker
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+systemctl enable docker
+systemctl enable docker.service
+systemctl enable containerd.service
 
 if docker > /dev/null
   then
