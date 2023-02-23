c29a5f6caa9851f67797c86b2a7344b66b74884c
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2023-02-23 17:06:01 +0800

    Relate # ! 提交將刪除sudo 以提供自動化測試
    修改項目:
    
    模組:

--- src/install-gitlab-ce.sh
+++ src/install-gitlab-ce.sh
@@ -14,33 +14,33 @@ docker run -d --publish 443:443 --publish 80:80 --publish 22:22 --publish 25:25 
 docker restart gitlab
 
 # Restore
-# sudo cp *_gitlab_backup.tar /var/lib/gitlab/data/backups/
-# sudo cp <備份版本>_gitlab_backup.tar /var/lib/gitlab/data/backups
+# cp *_gitlab_backup.tar /var/lib/gitlab/data/backups/
+# cp <備份版本>_gitlab_backup.tar /var/lib/gitlab/data/backups
 # docker exec -it gitlab bash
 # chown git:git /var/opt/gitlab/backups/<備份版本>_gitlab_backup.tar
 # exit
 # docker exec -it <name of container> gitlab-backup restore BACKUP=<備份版本>
-sudo cp gitlab.rb /var/lib/gitlab/config/
-# sudo cp gitlab-secrets.json /var/lib/gitlab/config/
+cp gitlab.rb /var/lib/gitlab/config/
+# cp gitlab-secrets.json /var/lib/gitlab/config/
 docker restart gitlab
 
 # Kubectl
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl
-sudo apt-get install -y apt-transport-https
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+apt-get update
+apt-get install -y ca-certificates curl
+apt-get install -y apt-transport-https
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
 
 # Minikube
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
-sudo dpkg -i minikube_latest_amd64.deb
+dpkg -i minikube_latest_amd64.deb
 minikube start --driver=docker
 minikube config set driver docker
 
 # Helm
-sudo snap install helm --classic
+snap install helm --classic
 helm repo add gitlab https://charts.gitlab.io
 helm repo update
 kubectl create namespace gitlab
