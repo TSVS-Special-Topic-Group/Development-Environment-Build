c735a72a41596e68fa9328ad2bb881b3c85522db
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2023-04-04 02:19:19 +0800

    Relate # ! 提交將更新版本與使用虛擬環境
    修改項目:
    
    模組:

--- src/auto-update-upgrade-backup.sh
+++ src/auto-update-upgrade-backup.sh
@@ -17,12 +17,12 @@ sudo ddclient -daemon=0 -debug -verbose -noquiet
 docker pull gitlab/gitlab-ce:latest
 docker pull gitlab/gitlab-runner:latest
 
-docker run --cpus=6 --cpuset-cpus 0-5 -d --publish 80:80 --publish 443:443 --publish 22:22 --publish 25:25 --publish 465:465 --publish 587:587 --name gitlab --restart always --volume /var/lib/gitlab/config/:/etc/gitlab --volume /var/lib/gitlab/logs/:/var/log/gitlab --volume /var/lib/gitlab/data/:/var/opt/gitlab gitlab/gitlab-ce:15.7.7-ce.0
+docker run --cpus=6 --cpuset-cpus 0-5 -d --publish 80:80 --publish 443:443 --publish 22:22 --publish 25:25 --publish 465:465 --publish 587:587 --name gitlab --restart always --volume /var/lib/gitlab/config/:/etc/gitlab --volume /var/lib/gitlab/logs/:/var/log/gitlab --volume /var/lib/gitlab/data/:/var/opt/gitlab gitlab/gitlab-ce:15.7.8-ce.0
 # docker run --cpus=1 --publish 80:80 --name -it -d atca python:latest
 docker run -d --name gitlab-runner --restart always -v /var/lib/gitlab-runner/:/etc/gitlab-runner/ -v /var/lib/gitlab-runner/certs/gitlab.atcatw.org.crt:/etc/gitlab-runner/certs/gitlab.atcatw.org.crt -v /var/run/docker.sock:/var/run/docker.sock gitlab/gitlab-runner:latest
 # gitlab-runner register --name gitlab-atcatw-org --url https://gitlab.atcatw.org --registration-token nRskPUYV-VVtTXFMtcKA
 
-minikube start --cpus 4 --force --force-systemd=true --driver=docker
+minikube start --cpus 4 --force --force-systemd=true --driver=podman
 kubectl create namespace gitlab
 helm repo update
 helm install --namespace gitlab gitlab-runner -f gitlab-values.yaml gitlab/gitlab-runner
