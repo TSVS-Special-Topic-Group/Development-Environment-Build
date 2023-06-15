5f0325622f398aade4e79d97bc6719c79f964812
Chenzhi Wu <qazzxcasdqwewsxedc@gmail.com>
2023-06-15 14:03:38 +0000

    Update file auto-update-upgrade-backup.sh

--- src/auto-update-upgrade-backup.sh
+++ src/auto-update-upgrade-backup.sh
@@ -20,7 +20,7 @@ docker pull gitlab/gitlab-runner:latest
 docker run --cpus=6 --cpuset-cpus 0-5 -d --publish 80:80 --publish 443:443 --publish 22:22 --publish 25:25 --publish 465:465 --publish 587:587 --name gitlab --restart always --volume /var/lib/gitlab/config/:/etc/gitlab --volume /var/lib/gitlab/logs/:/var/log/gitlab --volume /var/lib/gitlab/data/:/var/opt/gitlab gitlab/gitlab-ce:15.7.8-ce.0
 # docker run --cpus=1 --publish 80:80 --name -it -d atca python:latest
 docker run -d --name gitlab-runner --restart always -v /var/lib/gitlab-runner/:/etc/gitlab-runner/ -v /var/lib/gitlab-runner/certs/gitlab.atcatw.org.crt:/etc/gitlab-runner/certs/gitlab.atcatw.org.crt -v /var/run/docker.sock:/var/run/docker.sock gitlab/gitlab-runner:latest
-# gitlab-runner register --name gitlab-atcatw-org --url https://gitlab.atcatw.org --registration-token nRskPUYV-VVtTXFMtcKA
+# gitlab-runner register --name gitlab-atcatw-org --url https://gitlab.atcatw.org --registration-token xxxxxxxxxxxxx
 
 minikube start --cpus 6 --force --driver=podman
 kubectl create namespace gitlab
