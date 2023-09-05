7dc6bf288ea6e1af7098e35d9647f74e956909ca
timmy61109 <qazzxcasdqwewsxedc@gmail.com>
2023-09-05 13:21:00 +0800

    更新提供系統自動更新腳本

--- src/auto-update-upgrade-backup.sh
+++ src/auto-update-upgrade-backup.sh
@@ -1,31 +1,72 @@
 cd ~/Git/backup
 git pull
-helm repo add gitlab https://charts.gitlab.io
-docker exec -t gitlab gitlab-backup create  # 在更新前先進行備份
 
-docker stop gitlab gitlab-runner
-docker rm gitlab gitlab-runner
+docker exec -t gitlab rm -rf /var/lib/gitlab/data/backups/db
+docker exec -t gitlab rm -rf /var/lib/gitlab/data/backups/repositories
+docker exec -t gitlab rm /opt/gitlab/embedded/service/gitlab-rails/tmp/backup_restore.pid
+docker exec -t gitlab gitlab-backup create  # 在更新前先進行備份
+# sudo rsync -e 'ssh -p 2023' -arvhz --progress --delete /var/lib/gitlab/data/backups/ timmy@nas.atcatw.org:/mnt/NAS/backup/gitlab/  # 異地備援
+
+helm repo add gitlab https://charts.gitlab.io
+helm repo add traefik https://traefik.github.io/charts
+helm repo add filestash https://sebagarayco.github.io/helm-filestash
+helm search repo filestash
+
+docker stop gitlab
+docker rm gitlab
 helm delete gitlab-runner -n gitlab
 minikube stop
 minikube delete
 
+
 sudo apt-get update
 sudo apt-get upgrade -y
-flatpak update -y
-sudo ddclient -daemon=0 -debug -verbose -noquiet
-# sudo dpkg -i minikube_*.deb
+# flatpak update -y
+# sudo ddclient -daemon=0 -debug -verbose -noquiet
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+sudo dpkg -i minikube_latest_amd64.deb
 docker pull gitlab/gitlab-ce:latest
-docker pull gitlab/gitlab-runner:latest
 
-docker run --cpus=6 --cpuset-cpus 0-5 -d --publish 80:80 --publish 443:443 --publish 22:22 --publish 25:25 --publish 465:465 --publish 587:587 --name gitlab --restart always --volume /var/lib/gitlab/config/:/etc/gitlab --volume /var/lib/gitlab/logs/:/var/log/gitlab --volume /var/lib/gitlab/data/:/var/opt/gitlab gitlab/gitlab-ce:15.7.8-ce.0
-# docker run --cpus=1 --publish 80:80 --name -it -d atca python:latest
-docker run -d --name gitlab-runner --restart always -v /var/lib/gitlab-runner/:/etc/gitlab-runner/ -v /var/lib/gitlab-runner/certs/gitlab.atcatw.org.crt:/etc/gitlab-runner/certs/gitlab.atcatw.org.crt -v /var/run/docker.sock:/var/run/docker.sock gitlab/gitlab-runner:latest
-# gitlab-runner register --name gitlab-atcatw-org --url https://gitlab.atcatw.org --registration-token xxxxxxxxxxxxx
+docker run --cpus=6 --cpuset-cpus 0-5 -d \
+    --publish 8880:80 \
+    --publish 443:443 \
+    --publish 22:22 \
+    --publish 25:25 \
+    --publish 465:465 \
+    --publish 587:587 \
+    --name gitlab \
+    --restart always \
+    --volume /var/lib/gitlab/config/:/etc/gitlab \
+    --volume /var/lib/gitlab/logs/:/var/log/gitlab \
+    --volume /var/lib/gitlab/data/:/var/opt/gitlab \
+    gitlab/gitlab-ce:latest
 
-minikube start --cpus 6 --force --driver=podman
-kubectl create namespace gitlab
+# export HTTP_PROXY=http://10.0.0.3:3128
+# export HTTPS_PROXY=https://<proxy hostname:port>
+# export NO_PROXY=localhost,127.0.0.1,10.96.0.0/12,192.168.59.0/24,192.168.49.0/24,192.168.39.0/24,10.0.0.0/24,10.1.1.0/24,10.2.2.0/24,50.0.0.0/24
+
+minikube start --cpus 6 --force --driver=podman --container-runtime=cri-o --download-only=true
 helm repo update
-helm install --namespace gitlab gitlab-runner -f gitlab-values.yaml gitlab/gitlab-runner
+
+# podman machine init --cpus 2 --memory 2048 --disk-size 20
+# podman machine start
+# podman system connection default podman-machine-default-root
+# podman info
+
+kubectl create namespace gitlab
+# helm install --namespace gitlab gitlab-runner -f gitlab-values.yaml gitlab/gitlab-runner
 helm install --namespace gitlab atca-gitlab-runner -f atca-values.yaml gitlab/gitlab-runner
-helm upgrade --namespace gitlab gitlab-runner -f gitlab-values.yaml gitlab/gitlab-runner
+# helm upgrade --namespace gitlab gitlab-runner -f gitlab-values.yaml gitlab/gitlab-runner
 helm upgrade --namespace gitlab atca-gitlab-runner -f atca-values.yaml gitlab/gitlab-runner
+
+#helm search repo filestash
+# install
+#helm install filestash filestash/filestash \
+#  --namespace='filestash' --create-namespace \
+
+# kubectl create ns traefik-v2
+# Install in the namespace "traefik-v2"
+# helm install --namespace=traefik-v2 \
+#     traefik traefik/traefik
+
+# docker compose -f ispyagentdvr-docker-compose.yaml up -d --remove-orphans
